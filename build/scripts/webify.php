562dc293ce171f969e7a40c4b7f202799a250253
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-10-03 07:39:19 +0200

    Start documentation for PHPUnit 4.4 and PHPUnit 4.5

--- build/scripts/webify.php
+++ build/scripts/webify.php
@@ -20,17 +20,17 @@ function webify_directory($directory, $language, $version)
     );
 
     $editions  = array(
-      'en'    => array('4.3', '4.2', '3.7'),
+      'en'    => array('4.5', '4.4', '4.3', '3.7'),
       //'fr'    => array('4.2', '4.1', '3.7'),
-      'ja'    => array('4.3', '4.2', '3.7'),
+      'ja'    => array('4.5', '4.4', '4.3', '3.7'),
       //'pt_br' => array('4.2', '4.1', '3.7'),
-      'zh_cn' => array('4.3', '4.2', '3.7')
+      'zh_cn' => array('4.5', '4.4', '4.3', '3.7')
     );
 
     $old          = '3.7';
-    $stable       = '4.2';
-    $beta         = '4.3';
-    $alpha        = null;
+    $stable       = '4.3';
+    $beta         = '4.4';
+    $alpha        = '4.5';
     $languageList = '';
     $versionList  = '';
 
