fec2f9f2046d0158fd42ef25a5bca7991a6f60d1
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-08-04 08:11:58 +0200

    Start documentation for PHPUnit 6.4

--- build/scripts/webify.php
+++ build/scripts/webify.php
@@ -20,16 +20,16 @@ function webify_directory($directory, $language, $version)
     );
 
     $editions  = array(
-      'en'    => array('6.3', '6.2', '5.7'),
+      'en'    => array('6.4', '6.3', '5.7'),
       //'fr'    => array('4.2', '4.1'),
-      'ja'    => array('6.3', '6.2', '5.7'),
-      'pt_br' => array('6.3', '6.2', '5.7'),
-      'zh_cn' => array('6.3', '6.2', '5.7')
+      'ja'    => array('6.4', '6.3', '5.7'),
+      'pt_br' => array('6.4', '6.3', '5.7'),
+      'zh_cn' => array('6.4', '6.3', '5.7')
     );
 
     $old          = '5.7';
-    $stable       = '6.2';
-    $beta         = '6.3';
+    $stable       = '6.3';
+    $beta         = '6.4';
     $languageList = '';
     $versionList  = '';
 
